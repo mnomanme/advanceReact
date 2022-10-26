7282cc99b52fae3f63a346f9dab8c44b77bb3fc8
mnomanme <abdullah.noman083@gmail.com>
2022-10-26 15:00:59 +0600

    cleanup function :fire:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import UseEffectBasics from './components/UseEffectBasics';
+import UseEffectCleanup from './components/UseEffectCleanup';
 
 const App = () => {
 	return (
 		<div className="container">
 			<UseEffectBasics />
+			<UseEffectCleanup />
 		</div>
 	);
 };
--- src/components/UseEffectCleanup.js
+++ src/components/UseEffectCleanup.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const UseEffectCleanup = () => {
+	const [size, setSize] = useState(window.innerWidth);
+	console.log(size);
+
+	const checkSize = () => {
+		setSize(window.innerWidth);
+	};
+
+	useEffect(() => {
+		console.log('useEffect');
+		window.addEventListener('resize', checkSize);
+
+		// return () => {
+		// 	console.log('cleanup');
+		// 	window.removeEventListener('resize', checkSize);
+		// };
+	}, []);
+
+	console.log('render');
+
 	return (
 		<>
-			<h2>useEffect cleanup</h2>
+			<h3>useEffect cleanup</h3>
+			<h2>window</h2>
+			<h2>{size} PX</h2>
 		</>
 	);
 };
