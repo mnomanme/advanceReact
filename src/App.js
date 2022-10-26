a4262044f496b12e7057ce98216b173a545e57b0
mnomanme <abdullah.noman083@gmail.com>
2022-10-26 15:29:45 +0600

    fetch data :truck:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UseEffectBasics from './components/UseEffectBasics';
 import UseEffectCleanup from './components/UseEffectCleanup';
+import UseEffectFetchData from './components/UseEffectFetchData';
 
 const App = () => {
 	return (
 		<div className="container">
 			<UseEffectBasics />
 			<UseEffectCleanup />
+			<UseEffectFetchData />
 		</div>
 	);
 };
--- src/components/UseEffectFetchData.js
+++ src/components/UseEffectFetchData.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const url = `https://api.github.com/users`;
 
 const UseEffectFetchData = () => {
+	const [users, setUsers] = useState([]);
+
+	const getUsers = async () => {
+		const response = await fetch(url);
+		const users = await response.json();
+		setUsers(users);
+		console.log(users);
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
 	return (
 		<>
 			<h3>useEffect fetch data</h3>
+			<h2>github users</h2>
+			<ul className="users">
+				{users.map((user) => {
+					const { id, login, avatar_url, html_url } = user;
+					return (
+						<>
+							<li key={id}>
+								<img src={avatar_url} alt={login} />
+								<div>
+									<h4>{login}</h4>
+									<a href={html_url}>profile</a>
+								</div>
+							</li>
+						</>
+					);
+				})}
+			</ul>
 		</>
 	);
 };
