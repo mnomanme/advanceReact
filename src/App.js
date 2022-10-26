2e6e2495b50c9d91316b15e6f616406f880b4bdc
mnomanme <abdullah.noman083@gmail.com>
2022-10-26 18:35:56 +0600

    short circuit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MultipleReturns from './components/MultipleReturns';
+import ShortCircuit from './components/ShortCircuit';
 
 const App = () => {
 	return (
 		<div className="container">
 			<h2>Conditional Rendering</h2>
 			<MultipleReturns />
+			<ShortCircuit />
 		</div>
 	);
 };
--- src/components/ShortCircuit.js
+++ src/components/ShortCircuit.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ShortCircuit = () => {
+	const [text, setText] = useState('');
+	const firstValue = text || 'hello world';
+	const secondValue = text && 'hello world';
+
 	return (
 		<>
-			<h2>short circuit</h2>
+			<h3>short circuit</h3>
+			<h2>{firstValue}</h2>
+			<h2>value: {secondValue}</h2>
+			<h2>{text || 'Noman'}</h2>
+			{text && <h2>hello world</h2>}
+			{!text && <h2>hello world</h2>}
 		</>
 	);
 };
