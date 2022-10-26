0f29b4b44de95d6067eb57a261ef926e2d5c7b53
mnomanme <abdullah.noman083@gmail.com>
2022-10-26 18:12:54 +0600

    multiple returns :rocket:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import MultipleReturns from './components/MultipleReturns';
+
 const App = () => {
 	return (
 		<div className="container">
 			<h2>Conditional Rendering</h2>
+			<MultipleReturns />
 		</div>
 	);
 };
--- src/components/MultipleReturns.js
+++ src/components/MultipleReturns.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const url = `https://api.github.com/users/mnomanme`;
 
 const MultipleReturns = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+	const [user, setUser] = useState('default user');
+
+	useEffect(() => {
+		// setIsLoading(true);
+		fetch(url)
+			.then((res) => {
+				if (res.status >= 200 && res.status <= 299) {
+					return res.json();
+				} else {
+					setIsLoading(false);
+					setIsError(true);
+					throw new Error(res.statusText);
+				}
+			})
+			.then((user) => {
+				console.log(user);
+				const { login } = user;
+				setUser(login);
+				setIsError(false);
+				setIsLoading(false);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	if (isLoading) {
+		return (
+			<>
+				<h2>loading.....</h2>
+			</>
+		);
+	}
+	if (isError) {
+		return (
+			<>
+				<h2>Error.....</h2>
+			</>
+		);
+	}
 	return (
 		<>
 			<h3>multiple returns</h3>
+			<h2>{user}</h2>
 		</>
 	);
 };
