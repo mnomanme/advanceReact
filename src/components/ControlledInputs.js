2f325e6be8469be00743761a58cbe4246f4572c0
mnomanme <abdullah.noman083@gmail.com>
2022-10-27 15:11:22 +0600

    control input add to item :rocket:

--- src/components/ControlledInputs.js
+++ src/components/ControlledInputs.js
@@ -3,10 +3,27 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
 	const [firstName, setFirstName] = useState('');
 	const [email, setEmail] = useState('');
+	const [people, setPeople] = useState([]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log(firstName, email);
+
+		if (firstName && email) {
+			console.log('submit the form');
+
+			const person = { id: new Date().getTime().toString(), firstName, email };
+			console.log(person);
+
+			setPeople((people) => {
+				return [...people, person];
+			});
+
+			setFirstName('');
+			setEmail('');
+		} else {
+			console.log('empty values');
+		}
 	};
 
 	return (
@@ -34,10 +51,17 @@ const ControlledInputs = () => {
 							onChange={(e) => setEmail(e.target.value)}
 						/>
 					</div>
-					<button type="submit" onClick={handleSubmit}>
-						add person
-					</button>
+					<button type="submit">add person</button>
 				</form>
+				{people.map((person, index) => {
+					const { id, firstName, email } = person;
+					return (
+						<div key={id} className="item">
+							<h4>{firstName}</h4>
+							<p>{email}</p>
+						</div>
+					);
+				})}
 			</article>
 		</>
 	);
