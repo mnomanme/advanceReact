ff9b56e5290f63ac1dfbe7a669f76bc5942973f6
mnomanme <abdullah.noman083@gmail.com>
2022-10-27 16:03:24 +0600

    useRef preserve value works :fire:

--- src/components/UseRefBasics.js
+++ src/components/UseRefBasics.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const UseRefBasics = () => {
+	const refContainer = useRef(null);
+	const divContainer = useRef(null);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(refContainer.current.value);
+		console.log(divContainer.current);
+	};
+
+	console.log(refContainer);
+
+	useEffect(() => {
+		console.log(refContainer.current);
+		refContainer.current.focus();
+	});
+
 	return (
 		<>
 			<h3>useRef basics</h3>
+
+			<form className="form" onSubmit={handleSubmit}>
+				<div>
+					<input type="text" ref={refContainer} />
+					<button type="submit">submit</button>
+				</div>
+			</form>
+			<div ref={divContainer}>
+				<h2>hello world</h2>
+			</div>
 		</>
 	);
 };
