d2145803c5f013e6f8b867ee4b01ed1521723b3b
mnomanme <abdullah.noman083@gmail.com>
2022-10-31 15:48:05 +0600

    reducer add, remove dispatch

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import { data } from '../data';
 import Modal from './Modal';
+import { reducer } from './reducer';
+
+// reducer function
+
+const defaultState = {
+	people: [],
+	isModalOpen: false,
+	modalContent: '',
+};
 
 const Index = () => {
 	const [name, setName] = useState('');
-	const [people, setPeople] = useState(data);
-	const [showModal, setShowModal] = useState(false);
+	const [state, dispatch] = useReducer(reducer, defaultState);
+	// const [people, setPeople] = useState(data);
+	// const [showModal, setShowModal] = useState(false);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		if (name) {
-			setShowModal(true);
-			setPeople([...people, { id: new Date().getTime().toString(), name }]);
+			// setShowModal(true);
+			// setPeople([...people, { id: new Date().getTime().toString(), name }]);
+			// setName('');
+			const newItem = { id: new Date().getTime().toString(), name };
+			dispatch({ type: 'ADD_ITEM', payload: newItem });
 			setName('');
 		} else {
-			setShowModal(true);
+			// setShowModal(true);
+			dispatch({ type: 'NO_VALUE' });
 		}
+	};
+
+	const closeModal = () => {
+		dispatch({ type: 'CLOSE_MODAL' });
 	};
 
 	return (
 		<>
 			<h3>index</h3>
-			{showModal && <Modal />}
+			{/* {showModal && <Modal />} */}
+			{state.isModalOpen && (
+				<Modal closeModal={closeModal} modalContent={state.modalContent} />
+			)}
+
 			<form onSubmit={handleSubmit} className="form">
 				<div>
 					<input
@@ -34,11 +56,16 @@ const Index = () => {
 				<button type="submit">add</button>
 			</form>
 
-			{people.map((person) => {
+			{state.people.map((person) => {
 				const { id, name } = person;
 				return (
-					<div key={id}>
+					<div key={id} className="item">
 						<h4>{name}</h4>
+						<button
+							onClick={() => dispatch({ type: 'REMOVE_ITEM', payload: id })}
+						>
+							remove item
+						</button>
 					</div>
 				);
 			})}
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
 
-const Modal = () => {
+const Modal = ({ modalContent, closeModal }) => {
+	useEffect(() => {
+		setTimeout(() => {
+			closeModal();
+		}, 3000);
+	});
+
 	return (
 		<>
 			<h5>modal</h5>
+			<div className="modal">
+				<p>{modalContent}</p>
+			</div>
 		</>
 	);
 };
